ea5c4c625786b69a1e49492b08ccd40ebf4e80e5
JacobSmith1020 <jacobasmith1020@gmail.com>
2022-01-05 08:54:13 +0000

    Database local code working

--- app/models/login.rb
+++ app/models/login.rb
@@ -8,5 +8,13 @@ class Login
     end
 end
 
-#conn = PG.connect( :dbname => 'rmc_development_db', :user => 'admin', :password => 'team4', :host => 'localhost' )
-#conn.exec("\dt")
+# conn = PG.connect( :dbname => 'rmc_development_db', :user => 'admin', :password => 'team4', :host => 'localhost' )
+# sql_string = "CREATE TABLE IF NOT EXISTS \"user\" (\"username\" varchar(50) PRIMARY KEY NOT NULL, \"password\" varchar(50) NOT NULL, \"role\" varchar(15) NOT NULL);"
+# conn.exec(sql_string)
+# sql_string = "INSERT INTO \"user\" (\"username\", \"password\", \"role\") VALUES ('tony', 'password', 'student') ON CONFLICT DO NOTHING;"
+# conn.exec(sql_string)
+# conn.exec("SELECT * FROM \"user\";") do |result|
+#     result.each do |row|
+#         puts row.values_at("username", "password", "role")
+#     end
+# end
