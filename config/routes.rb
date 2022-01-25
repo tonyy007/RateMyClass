6ccfe5fae2a69a0bc4d3dd8c505ba13470843634
JacobSmith1020 <jacobasmith1020@gmail.com>
2022-01-25 18:34:30 +0000

    destory buttong working AND show and edit working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  #resources :users 
+  post 'login' => 'login#login', :as => 'login'
+  #root "login#login"
   root "users#index"
 end
