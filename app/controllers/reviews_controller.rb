87d453fba6e2e42308ec60d12219b99aba06a529
JacobSmith1020 <jacobasmith1020@gmail.com>
2022-02-17 01:45:04 +0000

    Disappearing reviews controller fixing function

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,106 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[ show edit update destroy ]
+
+  # GET /reviews or /reviews.json
+  def index
+    if params[:search] != nil #search bar used
+      params[:search].each do |value|
+        @searchval = "/" + value[1].to_s + "/"
+      end
+      @reviews = Review.all
+      @reviews_search = Array.new
+      @reviews.each do |review|
+        if (@searchval.match(review.course_code.to_s) != nil) or (@searchval.match(review.course_title.to_s) != nil) #choose what to sort by here
+          @reviews_search.append(review) 
+        end
+      end
+      @reviews = @reviews_search #from this list combine reviews whose course, prof, uni, etc are equal and display a set of matches to the user
+    else #search bar not used
+      @reviews = Review.order(:course_code, :course_title, :professor_name, :university_name)
+      @reviews_grouped = Array.new #outer layer array for set of grouped reviews of same type
+      @reviews_singular = Array.new #inner layer array for grouped reviews of same type
+      @i = 0
+      @reviews.each do |review|
+        if(@i == 0)
+          @reviews_singular.append(review)
+        elsif((review.course_code == @reviews_singular[@i - 1].course_code) and (review.course_title == @reviews_singular[@i - 1].course_title) and (review.professor_name == @reviews_singular[@i - 1].professor_name) and (review.university_name == @reviews_singular[@i - 1].university_name)) 
+          @reviews_singular.append(review)
+        else
+          @reviews_grouped.append(@reviews_singular.clone)
+          @reviews_singular.clear()
+          @reviews_singular.append(review)
+          @i = 0
+        end
+        @i += 1
+      end
+      @reviews_grouped.append(@reviews_singular.clone)
+      @reviews = @reviews_grouped
+      #@reviews = Review.all
+    end
+  end
+  
+  def indexlower
+  end
+
+  # GET /reviews/1 or /reviews/1.json
+  def show
+  end
+
+  # GET /reviews/new
+  def new
+    @review = Review.new
+  end
+
+  # GET /reviews/1/edit
+  def edit
+  end
+
+  # POST /reviews or /reviews.json
+  def create
+    @review = Review.new(review_params)
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
+        format.json { render :show, status: :created, location: @review }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /reviews/1 or /reviews/1.json
+  def update
+    respond_to do |format|
+      if @review.update(review_params)
+        format.html { redirect_to review_url(@review), notice: "Review was successfully updated." }
+        format.json { render :show, status: :ok, location: @review }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /reviews/1 or /reviews/1.json
+  def destroy
+    @review.destroy
+
+    respond_to do |format|
+      format.html { redirect_to reviews_url, notice: "Review was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def review_params
+      params.require(:review).permit(:workTime, :studyTime, :diffculty, :timeWish, :course_code, :course_title, :professor_name, :university_name)
+    end
+end
