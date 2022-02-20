b48b80a136946ee50af48db1053a3c475d631522
JacobSmith1020 <jacobasmith1020@gmail.com>
2022-02-20 23:40:17 +0000

    Attempting to push to heroku for migration

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,12 +6,24 @@ class CommentsController < ApplicationController
         redirect_to (indexlower_path + "?index=" + params[:comment][:index])
     end
     
-     def destroy
+    def destroy
         @review = Review.find(params[:review_id])
         @comment = @review.comments.find(params[:id])
         @comment.destroy
         redirect_to (indexlower_path + "?index=" + params[:index])
-     end
+    end
+    
+    # def update
+    #     respond_to do |format|
+    #       if @review.update(comment_params)
+    #         format.html { redirect_to (indexlower_path + "?index=" + params[:index]), notice: "Review was successfully updated." }
+    #         format.json { render :show, status: :ok, location: @review }
+    #       else
+    #         format.html { render :edit, status: :unprocessable_entity }
+    #         format.json { render json: @review.errors, status: :unprocessable_entity }
+    #       end
+    #     end
+    # end
 
     
     private
--- db/migrate/20220218214029_edit_notes_in_reviews.rb
+++
@@ -1,5 +0,0 @@
-class EditNotesInReviews < ActiveRecord::Migration[6.1]
-  def change
-    add_column :reviews, :notes, :text, limit: 300
-  end
-end
